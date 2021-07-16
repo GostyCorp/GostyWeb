a850b1b1a84ae11047ef47819f0ea332f6df876d
pApY-Dalix <dalix.playgamer@gmail.com>
2021-07-16 19:46:36 +0200

    Add Favicon

--- view/v_about.php
+++ view/v_about.php
@@ -1,8 +1,8 @@
 <div class="nine columns home">
 	<p>GOMIS YOANN</p>
 	<p>
-		Je suis un jeune développeur de 19 ans, actuellement en étude en BTS SIO option SLAM au lycée <a
-			href="http://www.lyc-curie.ac-aix-marseille.fr/" target="_blank">Marie Curie</a>.
+		Je suis un jeune développeur de 19 ans, actuellement en étude en BTS SIO option SLAM au lycée
+		<a href="http://www.lyc-curie.ac-aix-marseille.fr/" target="_blank">Marie Curie</a>.
 	</p>
 	<p>
 		Travailleur passionné en nouvelle technologie et programmation.
--- view/v_header.php
+++ view/v_header.php
@@ -3,17 +3,17 @@
 		<title>
 			lolyWeb
 		</title>
-		<link rel="icon"    href="favicon.ico">
-		<link rel="stylesheet"  href="css/normalize.css">
-		<link rel="stylesheet"  href="css/skeleton.css">
-		<link rel="stylesheet"  href="css/lolyweb.css">
+		<link rel="icon" href="img/NewCat.jpg">
+		<link rel="stylesheet" href="css/normalize.css">
+		<link rel="stylesheet" href="css/skeleton.css">
+		<link rel="stylesheet" href="css/lolyweb.css">
 		<meta charset="utf-16">
 	</head>
 	<body>
 		<div class="container">
 			<div class="row header">
 				<div class="three columns">
-					<img class="logo"	src="img/NewCat.jpg">
+					<img class="logo" src="img/NewCat.jpg">
 				</div>
 				<div class="nine columns">
 					<h1>ĽøĺýĆòřᵱ</h1>
